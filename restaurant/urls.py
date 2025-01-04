00a3b2acff9160cfd08b0c684803b284badf7062
Shabbir Habib <user@example.com>
2025-01-05 03:19:53 +0500

    serialized the menu model and created it's respective views and urls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,4 +2,14 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path(
+        "menu/",
+        views.MenuItemsView.as_view(),
+    ),
+    path(
+        "menu/<int:pk>",
+        views.SingleMenuItemView.as_view(),
+    ),
+]
